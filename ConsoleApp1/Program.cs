c3474c1cb2265074e2568443e930ae01dfd5580a
Byron Goldsack <byron.goldsack@aderant.com>
2025-01-31 16:49:34 +1300

    Refactor and upgrade to 20x20 matrix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,192 +10,80 @@ using ConsoleApp1;
 
 class Program
 {
-    int delayMs = 10;
-    int pollingDelayMs = 10;
-    double heatCoefficient = 0.5;
-    bool continueSending;
+
     static async Task Main(string[] args)
     {
+        var simulation = new ThermalSimulation();
+        simulation.BeginSimulation();
+
+        HttpListener httpListener = new HttpListener();
+        httpListener.Prefixes.Add("http://127.0.0.1:2024/");
+        httpListener.Start();
+        Console.WriteLine("Listening...");
+
         while (true)
         {
-
-            HttpListener httpListener = new HttpListener();
-            httpListener.Prefixes.Add("http://127.0.0.1:2024/");
-            httpListener.Start();
-            Console.WriteLine("Listening...");
-
-            Element[,] matrix = new Element[10, 10];
-            List<Thread> threads = new List<Thread>();
-
-            Random rnd = new Random();
-
-            Program p = new Program();
-            p.continueSending = true;
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            HttpListenerContext context = await httpListener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Element newEl = new Element();
-                    //newEl.currentTemp = rnd.NextDouble() * 100;
-                    newEl.currentTemp = 0;
-                    matrix[i, j] = newEl;
-                    Thread newThread = new Thread(() => p.PlateGo(newEl, p));
-                    threads.Add(newThread);
-                }
-            }
+                HttpListenerWebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+                Console.WriteLine("Websocket connection accepted");
+                WebSocket webSocket = wsContext.WebSocket;
 
-            for (int i = 0; i < matrix.GetLength(0); i++)
+                Thread Sender = new Thread(() => SendLoop(webSocket, simulation));
+                Sender.Start();
+
+                // Keep the connection open for further communication
+                await ReceiveLoop(webSocket, simulation);
+            }
+            else
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (i > 0)
-                    {
-                        matrix[i, j].AddNeighbour(matrix[i - 1, j]);
-                    }
-                    if (j > 0)
-                    {
-                        matrix[i, j].AddNeighbour(matrix[i, j - 1]);
-                    }
-                    if (i < matrix.GetLength(0) - 1)
-                    {
-                        matrix[i, j].AddNeighbour(matrix[i + 1, j]);
-                    }
-                    if (j < matrix.GetLength(1) - 1)
-                    {
-                        matrix[i, j].AddNeighbour(matrix[i, j + 1]);
-                    }
-                }
+                Console.WriteLine("Http request received");
+                HttpListenerRequest request = context.Request;
+
+                context.Response.StatusCode = 400;
+                context.Response.Close();
             }
+        }
+        simulation.Stop();
+    }
 
-            foreach (Thread t in threads)
-            {
-                t.Start();
-            }
-
-
-            while (true)
-            {
-                HttpListenerContext context = await httpListener.GetContextAsync();
-                if (context.Request.IsWebSocketRequest)
-                {
-                    HttpListenerWebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
-                    WebSocket webSocket = wsContext.WebSocket;
-
-                    Thread Sender = new Thread(() => p.SendLoop(webSocket, matrix, p));
-                    Sender.Start();
-
-                    // Keep the connection open for further communication
-                    await ReceiveLoop(webSocket, matrix, p);
-                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "1", CancellationToken.None);
-                    p.continueSending = false;
-                    httpListener.Stop();
-                    break;
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                    context.Response.Close();
-                }
-            }
-            foreach(Element e in matrix)
-            {
-                e.stopRequested = true;
-            }
+    static void SendLoop(WebSocket webSocket, ThermalSimulation sim)
+    {
+        while (sim.continueSending)
+        {
+            string message = sim.CreateMatrixRepresentation();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            //webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Binary, true, CancellationToken.None);
+            Thread.Sleep(sim.pollingDelayMs);
         }
     }
 
-    private static async Task ReceiveLoop(WebSocket webSocket, Element[,] matrix, Program p)
+    private static async Task ReceiveLoop(WebSocket webSocket, ThermalSimulation sim)
     {
-        while (webSocket.State == WebSocketState.Open)
+        while (sim.continueSending && webSocket.State == WebSocketState.Open)
         {
             byte[] buffer = new byte[1024 * 4];
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             string text = Encoding.Default.GetString(buffer);
             List<string> parameters = text.Split(':').ToList();
-            try
-            {
-                string variant = parameters[0];
 
-                if(variant == "coef")
+            if(parameters.Count > 1)
+            {
+                try
                 {
-                    p.heatCoefficient = double.Parse(parameters[1]) / 100;
-                    Console.WriteLine("coef change: " + p.heatCoefficient);
+                    sim.ParseParams(parameters);
                 }
-                else { 
-                    int x = int.Parse(parameters[1]);
-                    int y = int.Parse(parameters[2]);
-                    int temp = int.Parse(parameters[3]);
-                    int coef = int.Parse(parameters[4]);
-
-                    matrix[x, y].currentTemp = temp;
-            
-                    if(variant == "down")
-                    {
-                        matrix[x, y].isHeating = true;
-                    }
-                    else
-                    {
-                        foreach (Element e in matrix)
-                        {
-                            e.isHeating = false;
-                        }
-                        Console.WriteLine("reset holds");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                Console.WriteLine(text);
-                Console.WriteLine("Parameters: " + parameters);
-                Console.WriteLine("Parameters count: " + parameters.Count);
-            }
-
-        }
-        //await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-    }
-
-    void SendLoop(WebSocket webSocket, Element[,] matrix, Program p)
-    {
-        while (p.continueSending)
-        {
-            string message = CreateMatrixRepresentation(matrix);
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-            webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
-            Thread.Sleep(pollingDelayMs);
-        }
-    }
-
-    void PlateGo(Element e, Program p)
-    {
-        while (e.stopRequested == false)
-        {
-            if (!e.isHeating)
-            {
-                double av = 0;
-                foreach (Element e2 in e.neighbours)
+                catch (Exception ex)
                 {
-                    av += e2.GetTemperature();
+                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine(text);
+                    Console.WriteLine("Parameters: " + parameters);
+                    Console.WriteLine("Parameters count: " + parameters.Count);
                 }
-                av /= e.neighbours.Count;
-
-                e.currentTemp += (av - e.currentTemp) * p.heatCoefficient;
             }
-            //Console.WriteLine("adjusting temp to: " + e.currentTemp);
 
-            Thread.Sleep(delayMs);
         }
-    }
-
-    static string CreateMatrixRepresentation(Element[,] matrix)
-    {
-        string result = "";
-        foreach(Element e in matrix)
-        {
-            result += e.ToString();
-            result += ";";
-        }
-        return result;
     }
 }
--- ConsoleApp1/ThermalSimulation.cs
+++ ConsoleApp1/ThermalSimulation.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,147 @@ namespace ConsoleApp1
 {
     class ThermalSimulation
     {
+        public int delayMs = 10;
+        public int pollingDelayMs = 10;
+        public double heatCoefficient = 0.5;
+        public bool continueSending;
+        Element[,] matrix;
         public ThermalSimulation()
         {
+            matrix = new Element[20, 20];
+        }
 
+        public void BeginSimulation()
+        {
+
+            List<Thread> threads = new List<Thread>();
+
+            Random rnd = new Random();
+
+            continueSending = true;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Element newEl = new Element();
+                    newEl.currentTemp = 0;
+                    matrix[i, j] = newEl;
+                    Thread newThread = new Thread(() => PlateGo(newEl));
+                    threads.Add(newThread);
+                }
+            }
+
+            Console.WriteLine("Matrix created");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i > 0)
+                    {
+                        matrix[i, j].AddNeighbour(matrix[i - 1, j]);
+                    }
+                    if (j > 0)
+                    {
+                        matrix[i, j].AddNeighbour(matrix[i, j - 1]);
+                    }
+                    if (i < matrix.GetLength(0) - 1)
+                    {
+                        matrix[i, j].AddNeighbour(matrix[i + 1, j]);
+                    }
+                    if (j < matrix.GetLength(1) - 1)
+                    {
+                        matrix[i, j].AddNeighbour(matrix[i, j + 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Neighbours added");
+
+            int k = 0;
+            foreach (Thread t in threads)
+            {
+                t.Start();
+                Console.WriteLine("Thread " + k + " started");
+                k++;
+            }
+
+            Console.WriteLine("Threads started");
+
+        }
+
+        void PlateGo(Element e)
+        {
+            while (e.stopRequested == false)
+            {
+                if (!e.isHeating)
+                {
+                    double av = 0;
+                    foreach (Element e2 in e.neighbours)
+                    {
+                        av += e2.GetTemperature();
+                    }
+                    av /= e.neighbours.Count;
+
+                    e.currentTemp += (av - e.currentTemp) * heatCoefficient;
+                }
+                //Console.WriteLine("adjusting temp to: " + e.currentTemp);
+
+                Thread.Sleep(delayMs);
+            }
+        }
+
+        public string CreateMatrixRepresentation()
+        {
+            string result = "";
+            foreach (Element e in matrix)
+            {
+                result += e.ToString();
+                result += ";";
+            }
+            return result;
+        }
+
+        public void Stop()
+        {
+            foreach(Element e in matrix)
+            {
+                e.stopRequested = true;
+            }
+        }
+
+        public void ParseParams(List<string> parameters)
+        {
+            string variant = parameters[0];
+
+            if (variant == "coef")
+            {
+                heatCoefficient = double.Parse(parameters[1]) / 100;
+                Console.WriteLine("coef change: " + heatCoefficient);
+            }
+            else
+            {
+                int x = int.Parse(parameters[1]);
+                int y = int.Parse(parameters[2]);
+                int temp = int.Parse(parameters[3]);
+                int coef = int.Parse(parameters[4]);
+
+                matrix[x, y].currentTemp = temp;
+
+                if (variant == "down")
+                {
+                    matrix[x, y].isHeating = true;
+                }
+                else
+                {
+                    foreach (Element e in matrix)
+                    {
+                        e.isHeating = false;
+                    }
+                    Console.WriteLine("reset holds");
+                }
+            }
         }
     }
 }
